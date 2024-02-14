a864fe3d67261b1afee179c3afa3f0564a857f87
Yonghun <yonghuni484@gmail.com>
2024-02-14 22:52:23 +0900

    Design: Change login form design

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import * as Styled from "./Header.style";
 import useModalStore from "../../store/useModalStore";
+import { Button } from "@mui/material";
 
 const Header = () => {
   const modalState = useModalStore();
@@ -12,7 +13,10 @@ const Header = () => {
   return (
     <Styled.HeaderContainer>
       <div>철봉</div>
-      <button onClick={handleOpen}>로그인/회원가입</button>
+      {/* <button onClick={handleOpen}>로그인/회원가입</button> */}
+      <Button onClick={handleOpen} sx={{ color: "#333" }}>
+        로그인/회원가입
+      </Button>
     </Styled.HeaderContainer>
   );
 };
--- frontend/src/components/Input/Input.style.ts
+++ frontend/src/components/Input/Input.style.ts
@@ -1,5 +1,12 @@
 import styled from "@emotion/styled";
 
 export const Input = styled.input`
-  border: 1px solid red;
+  border: 1px solid #444;
+  border-radius: 0.2rem;
+
+  outline: none;
+
+  padding: 0.3rem;
+
+  font-family: inherit;
 `;
