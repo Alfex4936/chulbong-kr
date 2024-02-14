fb662028333aec30e9f582f2c9551a4a6a2ebfe0
Yonghun <yonghuni484@gmail.com>
2024-02-14 17:47:28 +0900

    Refactor: Migration to Typescript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
 import App from "./App";
+import "./style.css";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 const container = document.getElementById("root");
 
-const root = createRoot(container);
+const root = createRoot(container as HTMLElement);
 
 root.render(
   <React.StrictMode>
