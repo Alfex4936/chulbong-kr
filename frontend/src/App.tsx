620f5171290afcee0171e3b8ae49f8b8408a9de0
Yonghun <yonghuni484@gmail.com>
2024-02-16 00:51:28 +0900

    Feat: Add signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import Map from "./components/Map/Map";
+import { useEffect } from "react";
+import { Flip, ToastContainer, toast } from "react-toastify";
 import Header from "./components/Header/Header";
 import LoginForm from "./components/LoginForm/LoginForm";
+import Map from "./components/Map/Map";
 import BasicModal from "./components/Modal/Modal";
-import useModalStore from "./store/useModalStore";
 import SignupForm from "./components/SignupForm/SignupForm";
+import useModalStore from "./store/useModalStore";
+import useToastStore from "./store/useToastStore";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const modalState = useModalStore();
+  const toastState = useToastStore();
+
+  const notify = () => toast("회원가입 완료");
+
+  useEffect(() => {
+    if (toastState.isToast) {
+      notify();
+    }
+  }, [toastState.isToast]);
 
   return (
     <div>
@@ -22,6 +36,20 @@ const App = () => {
           <SignupForm />
         </BasicModal>
       )}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Flip}
+      />
     </div>
   );
 };
