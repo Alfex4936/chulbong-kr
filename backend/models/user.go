89abfecb1fcc116f0101e794df1fa0fe1789ef60
Alfex4936 <ikr@kakao.com>
2024-02-17 13:28:48 +0900

    [Backend] Get all markers api

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,15 +1,18 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 // User corresponds to the Users table in the database
 type User struct {
-	UserID       int       `json:"userId" db:"UserID"`
-	Username     string    `json:"username" db:"Username"`
-	Email        string    `json:"email" db:"Email"`
-	PasswordHash string    `json:"-" db:"PasswordHash"`                  // Can be empty for OAuth2 users
-	Provider     string    `json:"provider,omitempty" db:"Provider"`     // e.g., "google", "kakao"
-	ProviderID   string    `json:"providerId,omitempty" db:"ProviderID"` // Unique ID from the OAuth provider
-	CreatedAt    time.Time `json:"-" db:"CreatedAt"`
-	UpdatedAt    time.Time `json:"-" db:"UpdatedAt"`
+	UserID       int            `json:"userId" db:"UserID"`
+	Username     string         `json:"username" db:"Username"`
+	Email        string         `json:"email" db:"Email"`
+	PasswordHash sql.NullString `json:"-" db:"PasswordHash"`                  // Can be empty for OAuth2 users
+	Provider     sql.NullString `json:"provider,omitempty" db:"Provider"`     // e.g., "google", "kakao"
+	ProviderID   sql.NullString `json:"providerId,omitempty" db:"ProviderID"` // Unique ID from the OAuth provider
+	CreatedAt    time.Time      `json:"-" db:"CreatedAt"`
+	UpdatedAt    time.Time      `json:"-" db:"UpdatedAt"`
 }
