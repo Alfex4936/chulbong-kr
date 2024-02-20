e605ba958aa14e10ddecd3ae1f56da64e6adbced
Yonghun <2YH02>
2024-02-20 23:55:25 +0900

    Feat: Add Marker info modal

---
+++ frontend/src/components/MarkerInfoModal/MarkerInfoModal.style.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const imageWrap = styled.div`
+  & img {
+    object-fit: cover;
+    background-position: center;
+    background-size: cover;
+  }
+`;
+
+export const description = styled.div`
+  font-size: 1.3rem;
+  margin-top: 1rem;
+`;
