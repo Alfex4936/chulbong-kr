d3b4b8199ba9fef907b5bd06337ae774ae2c6d18
2YH02 <yonghuni484@gmail.com>
2024-02-29 00:14:26 +0900

    [Frontend] Refactor Font Optimization

--- frontend/src/components/SignupForm/SignupForm.tsx
+++ frontend/src/components/SignupForm/SignupForm.tsx
@@ -1,4 +1,5 @@
-import { Button, CircularProgress } from "@mui/material";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect, useState } from "react";
 import signin from "../../api/auth/signin";
 import useInput from "../../hooks/useInput";
