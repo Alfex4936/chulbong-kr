9c5861c45b0cdb73732bbf75bf7bc4e4e91e13df
Alfex4936 <ikr@kakao.com>
2024-02-29 11:39:37 +0900

    [Backend] Marker dislike API

---
+++ backend/models/marker_dislike.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+// MarkerDislike is a dislike
+type MarkerDislike struct {
+	DislikeID  int       `db:"DislikeID"`
+	MarkerID   int       `db:"MarkerID"`
+	UserID     int       `db:"UserID"`
+	DislikedAt time.Time `db:"DislikedAt"`
+}
