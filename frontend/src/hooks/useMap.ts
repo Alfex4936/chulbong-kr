788535c887ca7632d70c736fb46aa03c3ec6dcbf
2YH02 <yonghuni484@gmail.com>
2024-03-05 21:28:48 +0900

    [Frontend] Fix: refresh position reset

--- frontend/src/hooks/useMap.ts
+++ frontend/src/hooks/useMap.ts
@@ -1,19 +1,51 @@
 import type { KakaoMap } from "@/types/KakaoMap.types";
 import { MutableRefObject, useEffect, useState } from "react";
+import useMapPositionStore from "../store/useMapPositionStore";
 
 const useMap = (ref: MutableRefObject<HTMLDivElement | null>) => {
+  const positionState = useMapPositionStore();
+
   const [map, setMap] = useState<KakaoMap | null>(null);
 
   useEffect(() => {
-    const options = {
-      center: new window.kakao.maps.LatLng(37.566535, 126.9779692),
-      level: 5,
-      maxLevel: 12,
+    const setNewPosition = (lat: number, lng: number) => {
+      const options = {
+        center: new window.kakao.maps.LatLng(lat, lng),
+        level: 5,
+        maxLevel: 12,
+      };
+
+      const map = new window.kakao.maps.Map(ref.current, options);
+
+      window.kakao.maps.event.addListener(map, "dragend", () => {
+        const latlng = map.getCenter();
+
+        positionState.setPosition(latlng.getLat(), latlng.getLng());
+      });
+
+      setMap(map);
     };
 
-    const map = new window.kakao.maps.Map(ref.current, options);
-
-    setMap(map);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (positionState.lat === 0 || positionState.lng === 0) {
+            setNewPosition(position.coords.latitude, position.coords.longitude);
+          } else {
+            setNewPosition(positionState.lat, positionState.lng);
+          }
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    } else {
+      if (positionState.lat === 0 || positionState.lng === 0) {
+        setNewPosition(37.566535, 126.9779692);
+      } else {
+        setNewPosition(positionState.lat, positionState.lng);
+      }
+    }
   }, []);
 
   return map;
