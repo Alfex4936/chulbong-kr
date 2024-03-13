231a560d105cebf2a49992dff7d47be1aa1e19fc
2YH02 <yonghuni484@gmail.com>
2024-03-13 18:47:08 +0900

    [Frontend] Refactor remove refetch on window focus

--- frontend/src/hooks/mutation/marker/useDeleteMarker.ts
+++ frontend/src/hooks/mutation/marker/useDeleteMarker.ts
@@ -1,14 +1,10 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import deleteMarker from "../../../api/markers/deleteMarker";
 
 const useDeleteMarker = (id: number) => {
-  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: () => {
       return deleteMarker(id);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["marker", "all"] });
     },
   });
 };
--- frontend/src/hooks/mutation/marker/useUploadMarker.ts
+++ frontend/src/hooks/mutation/marker/useUploadMarker.ts
@@ -1,14 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
 import setNewMarker from "../../../api/markers/setNewMarker";
-import { useQueryClient } from "@tanstack/react-query";
 
 const useUploadMarker = () => {
-  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: setNewMarker,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["marker", "all"] });
-    },
   });
 };
 
