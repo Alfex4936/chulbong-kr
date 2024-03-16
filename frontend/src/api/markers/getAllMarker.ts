91aa7019e1f5573299b3d48a62af9d7c7b06ae89
2YH02 <yonghuni484@gmail.com>
2024-03-16 16:04:07 +0900

    [Frontend] Fix remove no cache

--- frontend/src/api/markers/getAllMarker.ts
+++ frontend/src/api/markers/getAllMarker.ts
@@ -5,11 +5,7 @@ export type MarkerRes = Pick<Marker, "markerId" | "latitude" | "longitude">;
 
 const getAllMarker = async (): Promise<MarkerRes[]> => {
   try {
-    const res = await axios.get(`/api/v1/markers`, {
-      headers: {
-        "Cache-Control": "no-cache",
-      },
-    });
+    const res = await axios.get(`/api/v1/markers`);
 
     return res.data;
   } catch (error) {
