bf16d0b9eb0f81423fa964cf44c4647478552021
2YH02 <yonghuni484@gmail.com>
2024-03-29 22:30:09 +0900

    [Frontend] Fix local https config develop

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,43 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import VitePluginHtmlEnv from "vite-plugin-html-env";
 import fs from "fs";
+
 // import mkcert from "vite-plugin-mkcert";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    // mkcert(),
-    VitePluginHtmlEnv(),
-    VitePluginHtmlEnv({ compiler: true }),
-  ],
-  server: {
-    // https: true,
-    https: {
-      key: fs.readFileSync("local.k-pullup.com-key.pem"),
-      cert: fs.readFileSync("local.k-pullup.com.pem"),
-    },
-    host: "local.k-pullup.com",
-    proxy: {
-      "/api/v1": {
-        target: "https://api.k-pullup.com",
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const isDevelop = process.env.VITE_DEVELOP === "true";
+
+  return defineConfig({
+    plugins: [
+      react(),
+      // mkcert(),
+      VitePluginHtmlEnv(),
+      VitePluginHtmlEnv({ compiler: true }),
+    ],
+    server: {
+      // https: true,
+      https: isDevelop
+        ? {
+            key: fs.readFileSync("local.k-pullup.com-key.pem"),
+            cert: fs.readFileSync("local.k-pullup.com.pem"),
+          }
+        : false,
+      host: "local.k-pullup.com",
+      proxy: {
+        "/api/v1": {
+          target: "https://api.k-pullup.com",
+          changeOrigin: true,
+          secure: false,
+        },
+        "/ws": {
+          target: "wss://api.k-pullup.com",
+          changeOrigin: true,
+          secure: false,
+        },
       },
-      "/ws": {
-        target: "wss://api.k-pullup.com",
-        changeOrigin: true,
-        secure: false,
-      },
     },
-  },
-});
+  });
+};
