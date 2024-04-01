e5ad9b60a68bfbc368c96fafebb0da0c34271895
2YH02 <yonghuni484@gmail.com>
2024-04-01 22:16:37 +0900

    [Frontend] Add alert throttling

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,11 +1,18 @@
 import axios, { AxiosInstance } from "axios";
 
 const baseInterceptors = (instance: AxiosInstance) => {
+  let isRateLimitAlertShown = false;
   instance.interceptors.response.use(
     (response) => response,
     (error) => {
-      if (error.response && error.response.status === 429) {
+      if (
+        error.response &&
+        error.response.status === 429 &&
+        !isRateLimitAlertShown
+      ) {
+        isRateLimitAlertShown = true;
         alert("요청이 너무 많습니다. 나중에 다시 시도해주세요.");
+        setTimeout(() => (isRateLimitAlertShown = false), 3000);
       }
       return Promise.reject(error);
     }
