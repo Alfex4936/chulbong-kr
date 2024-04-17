91930b4e55fdd86310a03c271bf5de06e6e48a2b
2YH02 <yonghuni484@gmail.com>
2024-04-17 16:51:31 +0900

    Feat: Add custom overlay and skeleton ui

--- frontend/src/api/markers/getAllMarker.ts
+++ frontend/src/api/markers/getAllMarker.ts
@@ -2,7 +2,10 @@ import { Marker } from "@/types/Marker.types";
 import instance from "../instance";
 
 // ${process.env.NEXT_PUBLIC_BASE_URL}
-export type MarkerRes = Pick<Marker, "markerId" | "latitude" | "longitude">;
+export type MarkerRes = Pick<
+  Marker,
+  "markerId" | "latitude" | "longitude" | "address"
+>;
 
 const getAllMarker = async (): Promise<MarkerRes[]> => {
   const res = await instance.get(`/api/v1/markers`);
