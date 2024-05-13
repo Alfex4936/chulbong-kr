5036a06f7e7542fb724963b3029c4888601ea48a
2YH02 <yonghuni484@gmail.com>
2024-05-14 00:23:11 +0900

    Feat: Add close marker search

--- frontend/src/app/(navbar)/search/page.tsx
+++ frontend/src/app/(navbar)/search/page.tsx
@@ -1,15 +1,17 @@
 import BlackSideBody from "@/components/atom/BlackSideBody";
 import ErrorMessage from "@/components/atom/ErrorMessage";
-import Heading from "@/components/atom/Heading";
+import PrevHeader from "@/components/atom/PrevHeader";
+import SearchRangebar from "./_components/SearchRangebar";
 
 const Search = () => {
   return (
-    <BlackSideBody toggle>
-      <Heading title="주변 검색" />
+    <BlackSideBody toggle bodyClass="p-0 mo:px-0 mo:pb-0">
+      <PrevHeader url="/home" text="주변 검색" />
       <ErrorMessage
         text="거리는 부정확할 수 있고, 현재 보이는 화면 중아에서부터 찾습니다."
         className="text-center"
       />
+      <SearchRangebar />
     </BlackSideBody>
   );
 };
