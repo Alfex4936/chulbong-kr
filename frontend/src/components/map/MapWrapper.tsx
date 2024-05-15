45a13e9e61c0b8ca5414c22ab93a3dee8dfd0165
2YH02 <yonghuni484@gmail.com>
2024-05-15 19:30:25 +0900

    Fix: Mobile responsive

--- frontend/src/components/map/MapWrapper.tsx
+++ frontend/src/components/map/MapWrapper.tsx
@@ -1,39 +1,20 @@
 "use client";
 
-import { MOBILE_WIDTH } from "@/constants";
 import useMiniMapStatusStore from "@/store/useMiniMapStatusStore";
 import useMobileMapOpenStore from "@/store/useMobileMapOpenStore";
 import useRoadviewStatusStore from "@/store/useRoadviewStatusStore";
 import Script from "next/script";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Map from "./Map";
 import MapLoading from "./MapLoading";
 import Roadview from "./Roadview";
 
 const MapWrapper = () => {
-  const { isOpen, open, close } = useMobileMapOpenStore();
+  const { isOpen } = useMobileMapOpenStore();
   const { isOpen: isRoadview } = useRoadviewStatusStore();
   const { setLoad } = useMiniMapStatusStore();
 
   const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > MOBILE_WIDTH) {
-        open();
-      } else {
-        close();
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <div
@@ -57,10 +38,6 @@ const MapWrapper = () => {
       {loaded && <Map />}
 
       {loaded && isRoadview && <Roadview />}
-
-      {!isOpen && (
-        <div className="absolute top-0 left-0 w-dvw h-dvh bg-black z-30" />
-      )}
     </div>
   );
 };
