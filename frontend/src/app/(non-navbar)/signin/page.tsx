2cf4376acf6e46295a604b855a3656aa620abd61
2YH02 <yonghuni484@gmail.com>
2024-05-18 16:37:31 +0900

    Fix: useSearchParams suspans error

--- frontend/src/app/(non-navbar)/signin/page.tsx
+++ frontend/src/app/(non-navbar)/signin/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import SigninBottomlinks from "./_components/SigninBottomlinks";
 import SigninForm from "./_components/signin-form";
 
@@ -12,7 +13,9 @@ const Signin = () => {
         대한민국 철봉 지도
       </Link>
       <div className="mx-auto w-full max-w-[500px] min-w-80 p-10">
-        <SigninForm />
+        <Suspense>
+          <SigninForm />
+        </Suspense>
       </div>
       <div className="mx-auto w-full max-w-[500px] min-w-80 px-10">
         <SigninBottomlinks />
