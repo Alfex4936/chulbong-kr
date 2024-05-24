b61888874a760d565c058dc5b1c275d3b7a5927a
2YH02 <yonghuni484@gmail.com>
2024-05-24 23:11:32 +0900

    Feat: Add main page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -122,6 +122,14 @@ const config = {
           "0%": { width: "0", height: "0", opacity: "0.8" },
           "100%": { width: "100px", height: "100px", opacity: "0" },
         },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
@@ -129,6 +137,7 @@ const config = {
         "slide-right": "slide-right .3s ease-in-out forwards",
         spotlight: "spotlight 2s ease .75s 1 forwards",
         ripple: "ripple 2s infinite",
+        shimmer: "shimmer 2s linear infinite",
       },
     },
   },
