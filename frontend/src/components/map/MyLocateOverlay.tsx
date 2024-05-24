24e02448a4717b1462ad470b7ef25e74793f182f
2YH02 <yonghuni484@gmail.com>
2024-05-24 17:42:21 +0900

    Fix: My locate overlay ripple effect

--- frontend/src/components/map/MyLocateOverlay.tsx
+++ frontend/src/components/map/MyLocateOverlay.tsx
@@ -1,6 +1,8 @@
 const MyLocateOverlay = () => {
   return (
-    <div className="absolute w-4 h-4 bg-red border border-red rounded-full transform -translate-x-1/2 -translate-y-1/2 ripple" />
+    <div className="w-4 h-4 bg-rose-500 border border-red rounded-full transform -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-0 w-0 border border-rose-500 border-solid rounded-full opacity-0 animate-ripple " />
+    </div>
   );
 };
 
