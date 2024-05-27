ff315f434d5b28a69ac87e31dac7e4334a7d3caf
2YH02 <yonghuni484@gmail.com>
2024-05-28 00:00:15 +0900

    Fix: Change cross origin

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-// import withPWA from "next-pwa";
-
-// const nextPWA = withPWA({
-//   dest: "public",
-// });
-
 const nextConfig = {
+  crossOrigin: "use-credentials",
   reactStrictMode: false,
   async rewrites() {
     return [
@@ -15,30 +10,6 @@ const nextConfig = {
         destination: "https://api.k-pullup.com/api/v1/:path*",
       },
     ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/manifest.webmanifest",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type",
-          },
-        ],
-      },
-    ];
-  },
-  images: {
-    domains: ["chulbong-kr.s3.amazonaws.com"],
   },
 };
 
