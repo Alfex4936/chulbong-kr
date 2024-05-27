4acd884b15a0aba796dcf09a8c9e18c9459f5093
2YH02 <yonghuni484@gmail.com>
2024-05-27 20:02:23 +0900

    Fix: Change page loading ui

--- frontend/src/components/atom/BlackSideBody.tsx
+++ frontend/src/components/atom/BlackSideBody.tsx
@@ -6,7 +6,8 @@ import usePageLoadingStore from "@/store/usePageLoadingStore";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { ArrowLeftIcon, ArrowRightIcon } from "../icons/ArrowIcons";
-import PageLoadingBar from "../layout/PageLoadingBar";
+import LoadingSpinner from "./LoadingSpinner";
+// import PageLoadingBar from "../layout/PageLoadingBar";
 
 type Props = {
   children: React.ReactNode;
@@ -52,26 +53,34 @@ const BlackSideBody = ({ children, toggle, bodyClass, className }: Props) => {
         className
       )}
     >
-      <div
-        className={cn(
-          `h-full overflow-auto scrollbar-thin pb-4 mo:pb-20 mo:px-4 ${
-            isOpen ? "px-4" : "px-0"
-          }`,
-          bodyClass
-        )}
-      >
-        <PageLoadingBar />
-        {children}
-      </div>
+      {isLoading ? (
+        <div className="h-full flex justify-center items-center">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <>
+          <div
+            className={cn(
+              `h-full overflow-auto scrollbar-thin pb-4 mo:pb-20 mo:px-4 ${
+                isOpen ? "px-4" : "px-0"
+              }`,
+              bodyClass
+            )}
+          >
+            {/* <PageLoadingBar /> */}
+            {children}
+          </div>
 
-      {toggle && (
-        <button
-          className="absolute -right-9 top-1/2 -translate-y-1/2 z-50
-                  bg-black-light py-3 rounded-md shadow-md mo:hidden"
-          onClick={() => open()}
-        >
-          {isOpen ? <ArrowLeftIcon /> : <ArrowRightIcon />}
-        </button>
+          {toggle && (
+            <button
+              className="absolute -right-9 top-1/2 -translate-y-1/2 z-50
+            bg-black-light py-3 rounded-md shadow-md mo:hidden"
+              onClick={() => open()}
+            >
+              {isOpen ? <ArrowLeftIcon /> : <ArrowRightIcon />}
+            </button>
+          )}
+        </>
       )}
     </div>
   );
