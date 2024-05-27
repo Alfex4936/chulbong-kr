4bc7f6125bff739e05a65882b03f53bdda63d2cc
2YH02 <yonghuni484@gmail.com>
2024-05-27 22:18:34 +0900

    Fix: PWA manifest file

--- frontend/src/app/(non-navbar)/layout.tsx
+++ frontend/src/app/(non-navbar)/layout.tsx
@@ -12,6 +12,7 @@ const nanum = Nanum_Gothic({
 });
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_URL as string),
+  // manifest: "/manifest.json",
   title: "대한민국 철봉 지도",
   keywords: "철봉지도,위치등록,철봉정보,채팅,위치검색,관리,철봉찾기",
   description:
@@ -64,3 +65,22 @@ const RootLayout = ({
 };
 
 export default RootLayout;
+
+// {
+//   "theme_color": "#222",
+//   "background_color": "#222",
+//   "display": "standalone",
+//   "scope": "/",
+//   "start_url": "/",
+//   "name": "철봉 지도",
+//   "short_name": "철봉 지도",
+//   "description": "주변 철봉 위치를 확인하세요",
+//   "lang": "ko-KR",
+//   "icons": [
+//     {
+//       "src": "/2.png",
+//       "sizes": "192x192",
+//       "type": "image/png"
+//     }
+//   ]
+// }
