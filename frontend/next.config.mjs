6bc74d2928e4f1eb0021e1d273c7157bab7cf3dc
2YH02 <yonghuni484@gmail.com>
2024-05-27 23:10:01 +0900

    Fix: PWA manifest file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -23,3 +23,33 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+// import { MetadataRoute } from "next";
+
+// const manifest = (): MetadataRoute.Manifest => {
+//   return {
+//     theme_color: "#222",
+//     background_color: "#222",
+//     display: "standalone",
+//     scope: "/",
+//     start_url: "/",
+//     name: "철봉 지도",
+//     short_name: "철봉 지도",
+//     description: "주변 철봉 위치를 확인하세요",
+//     lang: "ko-KR",
+//     icons: [
+//       {
+//         src: "/logo192.png",
+//         sizes: "192x192",
+//         type: "image/png",
+//         purpose: "maskable",
+//       },
+//       {
+//         src: "/logo512.png",
+//         sizes: "512x512",
+//         type: "image/png",
+//       },
+//     ],
+//   };
+// };
+// export default manifest;
