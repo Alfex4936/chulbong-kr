32349971f394940f4f9bdbc7a2f40ec8c0b8e9af
2YH02 <yonghuni484@gmail.com>
2024-05-30 15:58:40 +0900

    Feat: Add Cancel signup button

--- frontend/src/app/(non-navbar)/signin/page.tsx
+++ frontend/src/app/(non-navbar)/signin/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 import SigninBottomlinks from "./_components/SigninBottomlinks";
@@ -6,12 +7,12 @@ import SigninForm from "./_components/signin-form";
 const Signin = () => {
   return (
     <div>
-      <Link
-        href={"/home"}
-        className="inline-block w-full text-center text-2xl mt-14"
-      >
-        대한민국 철봉 지도
-      </Link>
+      <div className="flex items-center justify-center mt-14 w-full">
+        <Image src={"/2.png"} alt="logo" width={45} height={45} className="mr-2" />
+        <Link href={"/home"} className="text-2xl">
+          대한민국 철봉 지도
+        </Link>
+      </div>
       <div className="mx-auto w-full max-w-[500px] min-w-80 p-10">
         <Suspense>
           <SigninForm />
