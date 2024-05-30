f2e38ee07e51099cb08cbb97c6c88362ae472b42
2YH02 <yonghuni484@gmail.com>
2024-05-30 14:05:30 +0900

    Feat: Add mobile map button animation and icon

--- frontend/src/components/common/MapButton.tsx
+++ frontend/src/components/common/MapButton.tsx
@@ -2,13 +2,15 @@
 
 import useMobileMapOpenStore from "@/store/useMobileMapOpenStore";
 import { ComponentProps } from "react";
+import { FaMapLocationDot } from "react-icons/fa6";
+import MapCloseButton from "./MapCloseButton";
 
 interface Props extends ComponentProps<"button"> {
   selected: boolean;
 }
 
 const MapButton = ({ selected, ...props }: Props) => {
-  const { toggle } = useMobileMapOpenStore();
+  const { toggle, isOpen } = useMobileMapOpenStore();
 
   return (
     <button
@@ -16,7 +18,7 @@ const MapButton = ({ selected, ...props }: Props) => {
       {...props}
       onClick={toggle}
     >
-      맵
+      {isOpen ? <MapCloseButton /> : <FaMapLocationDot className="text-2xl" />}
     </button>
   );
 };
